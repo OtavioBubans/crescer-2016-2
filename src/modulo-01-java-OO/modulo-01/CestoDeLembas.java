9413090fffb8b790d48057de67ab2c7f99409e7d
Unknown <otaviobubans@hotmail.com>
2016-09-29 12:22:47 -0300

    feat(CestoDeLembas) : implementei novo metodo de dividir Lembas

--- src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -7,25 +7,26 @@ public class CestoDeLembas{
         this.quantidade=quantidade;
     }
     
-    public boolean dividirLembas(int lembas){
-        if( podeDividir(lembas) == true && lembas >= 2 && lembas <=100)
-            return true;
-        else{
-            return false;
-        }
-    
-         
+     public boolean podeDividirEmPares() {
+        return quantidade > 2 && quantidade % 2 == 0 && quantidade <= 100;
     }
     
+    
+    
+       /** public boolean dividirLembas(int lembas){
+            if( podeDividir(lembas) == true && lembas >= 2 && lembas <=100)
+                return true;
+            else{
+                return false;
+            }
+        }     
     public boolean podeDividir( int n){
         if( n % 2 == 0){
             return true;
         }else{ 
             return false;
         }
-    }
+    }*/
     
-    public int getQuantidade(){
-        return quantidade;
-    }
+
 }
