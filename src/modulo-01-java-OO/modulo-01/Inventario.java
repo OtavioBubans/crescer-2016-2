4921969c76ad0785cf931a90f533e954beddebbc
Otávio Fabrin Bubans <otaviobubans@hotmail.com>
2016-09-29 23:16:40 -0300

    feat(Inventario) metodos de adicionar e remover item

--- src/modulo-01-java-OO/modulo-01/Inventario.java
+++ src/modulo-01-java-OO/modulo-01/Inventario.java
@@ -1,11 +1,40 @@
+
+import java.util.ArrayList;
+
+
 public class Inventario{
+      
+    ArrayList<Item> itens = new ArrayList<>();
     
-    public void adicionarItem(){
+    
+    public void adicionarItem(Item item){
+        itens.add(new Item());
+    }
+    
+    public void removerItem(Item item){
+        
+        for(int i = 0; i < itens.size(); i++){
+            if(item.equals(itens.get(i))){
+                itens.remove(i);
+                return;
+            }
+        }
         
     }
     
-    public void removerItem(){
+    
+   /** public void rodar() {
+        ArrayList<Item> itens = new ArrayList<>();
+        itens.add(new Item("Espada Z", 1));
+        itens.add(new Item("Semente dos Deuses", 56));
+        itens.add(new Item("Nuvem voadora", 1));
         
-    }
+        for (int i = 0; i < itens.size(); i++) {
+            Item itemAtual = itens.get(i);
+            System.out.println(itemAtual.getDescricao());
+        }
+        
+    } */
+   
     
 }
