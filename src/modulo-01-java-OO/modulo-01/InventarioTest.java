05c7d04a606141b012accbfb95952a165a5160de
Otávio Fabrin Bubans <otaviobubans@hotmail.com>
2016-09-30 00:48:34 -0300

    test(InventarioTest): correção de testes

--- src/modulo-01-java-OO/modulo-01/InventarioTest.java
+++ src/modulo-01-java-OO/modulo-01/InventarioTest.java
@@ -14,7 +14,7 @@ public class InventarioTest{
     public void addUmItem(){
         
         Inventario inventario = new Inventario();
-        Item item = new Item();
+        Item item = new Item("Espada",4);
         
         inventario.adicionarItem(item);
         
@@ -25,13 +25,17 @@ public class InventarioTest{
     public void add5Iten(){
         
         Inventario inventario = new Inventario();
-        Item item = new Item();
+        Item item1 = new Item("Espada",4);
+        Item item2 = new Item("Escudo",4);
+        Item item3 = new Item("Machado",6);
+        Item item4 = new Item("Armadura",2);
+        Item item5 = new Item("Capa",25);
         
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
+        inventario.adicionarItem(item1);
+        inventario.adicionarItem(item2);
+        inventario.adicionarItem(item3);
+        inventario.adicionarItem(item4);
+        inventario.adicionarItem(item5);
         
         assertEquals(5, inventario.getTamanho());
     }
@@ -39,7 +43,7 @@ public class InventarioTest{
     @Test
     public void addERemove1Item(){
         Inventario inventario = new Inventario();
-        Item item = new Item();
+        Item item = new Item("Espada",5);
         
         inventario.adicionarItem(item);
         inventario.removerItem(item);
@@ -50,15 +54,19 @@ public class InventarioTest{
     public void add5ItensRemove2(){
         
         Inventario inventario = new Inventario();
-        Item item = new Item();
+        Item item1 = new Item("Espada",4);
+        Item item2 = new Item("Escudo",4);
+        Item item3 = new Item("Machado",6);
+        Item item4 = new Item("Armadura",2);
+        Item item5 = new Item("Capa",25);
         
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.removerItem(item);
-        inventario.removerItem(item);
+        inventario.adicionarItem(item1);
+        inventario.adicionarItem(item2);
+        inventario.adicionarItem(item3);
+        inventario.adicionarItem(item4);
+        inventario.adicionarItem(item5);
+        inventario.removerItem(item1);
+        inventario.removerItem(item3);
         assertEquals(3, inventario.getTamanho());
     }
     
@@ -66,25 +74,29 @@ public class InventarioTest{
     public void add5ItensRemove5(){
         
         Inventario inventario = new Inventario();
-        Item item = new Item();
+        Item item1 = new Item("Espada",4);
+        Item item2 = new Item("Escudo",4);
+        Item item3 = new Item("Machado",6);
+        Item item4 = new Item("Armadura",2);
+        Item item5 = new Item("Capa",25);
         
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.adicionarItem(item);
-        inventario.removerItem(item);
-        inventario.removerItem(item);
-        inventario.removerItem(item);
-        inventario.removerItem(item);
-        inventario.removerItem(item);
+        inventario.adicionarItem(item1);
+        inventario.adicionarItem(item2);
+        inventario.adicionarItem(item3);
+        inventario.adicionarItem(item4);
+        inventario.adicionarItem(item5);
+        inventario.removerItem(item1);
+        inventario.removerItem(item2);
+        inventario.removerItem(item3);
+        inventario.removerItem(item4);
+        inventario.removerItem(item5);
         assertEquals(0, inventario.getTamanho());
     }
     
     @Test
     public void removeInventarioVazio(){
         Inventario inventario = new Inventario();
-        Item item = new Item();
+        Item item = new Item("Espada",4);
         
         inventario.removerItem(item);
         assertEquals(0, inventario.getTamanho());
