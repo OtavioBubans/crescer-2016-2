0c14308da466cce74e78b9c48d4a31fb83166265
Otávio Bubans <otaviobubans@hotmail.com>
2016-10-31 17:48:50 -0200

    feat(.Net)

---
+++ src/modulo-05- .NET/Exercicio1/ConsoleApplication1/ConsoleApplication1/Corredor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public interface Corredor
+    {
+        string Nome { get; }
+        Enum Nivel { get; }
+    }
+}
