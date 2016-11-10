7e6c5cacce0a622f9986bfefe1f5f722d49f36f1
Otávio Bubans <otaviobubans@hotmail.com>
2016-11-10 17:50:33 -0200

    feat(MarvelFilx):conteudo sobre ajax, passado em aula

---
+++ src/modulo-06-ajax/Marvelflix/Marvelflix/Scripts/app/telaPrincipal.js
@@ -0,0 +1,41 @@
+class TelaPrincipal {
+  
+  constructor(seletor) {
+    this.$elem = $(seletor);
+    this.registrarBindsEventos();
+    this.renderizarEstadoInicial();
+  }
+
+    registrarBindsEventos() {
+
+
+  }
+
+  renderizarEstadoInicial() {
+    $('.tela-centralizada').removeClass('tela-centralizada');
+    this.$elem.show();
+
+    $.get('/api/herois')
+      .done((res) => {
+          marvelflix.render('.tela', 'tela-principal', {
+              chars: res.map(function (item) {
+                  return {
+                      id: item.id,
+                      name: item.nome,
+                      thumbnail: item.urlThumbnail
+                  }
+              })
+          });
+      });
+
+   
+ /*   marvelflix.render('.tela', 'tela-principal', {
+      chars: [
+        { id: 1, name: 'Iron Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
+        { id: 2, name: 'Spider Man', thumbnail: 'https://cloud.githubusercontent.com/assets/526075/20180159/652c5846-a740-11e6-8b56-19256fcc52c3.png' },
+      ]
+    });  */
+
+  }
+
+}
